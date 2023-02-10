6451e5dc797e0ffcc45002c4c0d714d9ef55c5b7
Alif <alifaziz@gmail.com>
2023-02-10 19:24:19 +0800

    Add bg image

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,15 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const [restaurants, setRestaurants] = useState<RestaurantData[]>([]);
   const [randomRestaurant, setRandomRestaurant] = useState("Ready?");
+  const [randomRestaurantImage, setRandomRestaurantImage] = useState("");
+  const [randomRestaurantDescription, setRandomRestaurantDescription] = useState("");
   let dataFetchedRef = false;
 
   interface RestaurantData {
     id: number;
     name: string;
+    image: string;
+    description: string;
   }
 
   const randomize = () => {
@@ -25,9 +29,10 @@ export default function Home() {
     }
     setTimeout(() => {
       setRandomRestaurant(restaurants[random].name);
+      setRandomRestaurantImage(restaurants[random].image);
+      setRandomRestaurantDescription(restaurants[random].description);
       setIsLoading(false);
-    }
-    , 1200);
+    }, 1200);
   };
 
   useEffect(() => {
@@ -36,6 +41,16 @@ export default function Home() {
     const restaurants: RestaurantData[] = require("../data/restaurant.json");
     setRestaurants(restaurants);
   }, []);
+
+  const setStyles = (imageUrl: string) => {
+    return {
+      backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${imageUrl})`,
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      opacity: 0.2,
+    };
+  };
 
   return (
     <>
@@ -46,11 +61,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div
-        className="container mx-auto p-2"
+        className="container mx-auto p-2 bg-cover bg-center bg-no-repeat"
         onClick={randomize}
+        style={setStyles(randomRestaurantImage)}
       >
         <div className="flex flex-col h-screen items-center justify-center">
-          <div className="opacity-80 inline-flex text-center transition-opacity mb-3">
+          <div className="inline-flex text-center transition-opacity mb-3">
             {isLoading ? (
               <PropagateLoader
                 color="#fff"
@@ -58,10 +74,18 @@ export default function Home() {
                 speedMultiplier={2}
               />
             ) : (
-              <span className="drop-shadow-md">{randomRestaurant}</span>
+              <div>
+                <span className="drop-shadow-md block">{randomRestaurant}</span>
+                <span className="text-sm opacity-60">{randomRestaurantDescription || "Tap"}</span>
+              </div>
             )}
           </div>
         </div>
+        <footer className="fixed bottom-0 w-full text-center p-4">
+          <span className="text-xs opacity-20 font-thin">
+            MakanMana by @h4ck4life
+          </span>
+        </footer>
       </div>
     </>
   );
